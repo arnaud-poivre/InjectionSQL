341c7ffcfba58a461d7373967f572d7c3030301f
arnaud-poivre <77829381+arnaud-poivre@users.noreply.github.com>
2021-10-19 08:59:45 +0200

    Update injectionTest.php

--- injectionTest.php
+++ injectionTest.php
@@ -8,10 +8,12 @@ final class injectionTest extends TestCase
 {
     public function injection_getMdp_DevraitAvoirUnSeulMdp() : void {
         //AAA
+        //Ici l'injection fonctionne donc le test devrait etre faux.
     }
 
     public function correctionInjection_getMdp_DevraitAvoirUnSeulMdp() : void {
         //AAA
+        //Correction de l'injection nous récuperons donc le mot de passe attendu et non tous les mots de passes de la table.
     }
 }
 ?>
